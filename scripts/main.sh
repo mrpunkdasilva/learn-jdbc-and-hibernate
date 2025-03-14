744bbc03138a6cba2dd2b6e995402bae9dc354bb
Mr Punk da Silva <gustavojs417@gmail.com>
2025-03-14 18:30:20 -0300

    Atualização automática do repositório - 2025-03-14 18:30:20

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -e
+
+echo "Diretório atual: $(pwd)"
+
+echo "Unzipping writerside..."
 ./unzip_writerside.sh
+
+echo "Pushing to remote repository..."
 ./push_remote_repo.sh
 
+echo "Finished running scripts."
